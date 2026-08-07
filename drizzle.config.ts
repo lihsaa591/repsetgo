7d67dfa87a89399c2fbc2e2e3b4dbc210d5eb779
Aashil <bijuksheashil@gmail.com>
2026-08-07 23:07:22 +0545

    feat: add drizzle client and config

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: [
+    "./src/lib/server/auth/schema.ts",
+    "./src/lib/server/workouts/schema.ts",
+    "./src/lib/server/exercises/schema.ts",
+  ],
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
